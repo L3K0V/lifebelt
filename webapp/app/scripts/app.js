8df6fb0a526a242b4710a901f9adcfc93dee748c
Alexander Chernaev <alchernaev@gmail.com>
2015-08-02 21:29:08 +0300

    Added separate ui-views for drawer, header and content

--- webapp/app/scripts/app.js
+++ webapp/app/scripts/app.js
@@ -16,6 +16,15 @@ angular.module("lifebeltApp", [
 			"": {
 				templateUrl: "scripts/frame/frame.html",
 				controller: "MDLLoaderCtrl as mdlLoaderCtrl"
+			},
+			"drawer@home": {
+				templateUrl: "scripts/frame/default-drawer.html"
+			},
+			"header@home": {
+				templateUrl: "scripts/frame/default-header.html"
+			},
+			"content@home": {
+				templateUrl: "scripts/frame/default-content.html"
 			}
 		}
 	});
