7de40b23d7630ef3ed9f2b541fb972ef78930143
Asen Lekov <asenlekoff@gmail.com>
2015-08-10 23:11:51 +0300

    Django unleashed

--- server/lifebelt/settings.py
+++ server/lifebelt/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lifebelt.apps.core.users',
+    'lifebelt.apps.core.courses'
 )
 
 MIDDLEWARE_CLASSES = (
