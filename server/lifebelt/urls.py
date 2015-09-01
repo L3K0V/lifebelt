ac7fe6ba11f9d72b5703e404fd3df88dd4a62504
Alexander Chernaev <alchernaev@gmail.com>
2015-09-01 20:21:48 +0300

    Fixed static and media urls

--- server/lifebelt/urls.py
+++ server/lifebelt/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += patterns('django.views.static',
-                            (r'files/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}),)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
     urlpatterns += [url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')), ]
