4b7a457cbd97612c97120063728d6e23aa4c9b48
Asen Lekov <asenlekoff@gmail.com>
2015-09-24 23:26:18 +0300

    Heroku deploy configuration

--- server/lifebelt/settings/base.py
+++ server/lifebelt/settings/base.py
@@ -16,12 +16,13 @@ import datetime
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+SECRET_KEY = '?5o;gAeZpe!u3ISOt7|)#Tx4GneshtodrugoJ2D8zI'
 
 ALLOWED_HOSTS = []
 
@@ -31,7 +32,7 @@ CVS_MEMBERS_IMPORT_FORMAT = {
     'first_name': 'Име',
     'last_name': 'Фамилия',
     'email': 'Имейл',
-    'github': 'Github',
+    'github': 'GitHub',
     'student_class': 'Паралелка',
     'student_grade': 'Клас',
     'student_number': 'Номер'
@@ -133,6 +134,30 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'userfiles')
 MEDIA_URL = '/files/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../static'),
-    os.path.join(BASE_DIR, '../../webapp/app')
+    # os.path.join(BASE_DIR, '../static'),
+    os.path.join(BASE_DIR, '../../webapp/dist'),
 )
+
+EMAIL_ENROLL_TEMPLATE = """Здрасти,
+<p>Ти беше записан <b>успешно</b> за курса по <a href="{lifebelt}">{course} ({year})</a> и затова ти изпращаме малко линкове и информация.</p>
+<br>
+<p>Преди всичко - това е твоята парола и не я казвай на никого: <strong>{pwd}</strong></p>
+<br>
+<p><a href="{headquarters}">Тук</a> можеш да видиш за това кога си присъствал и забележки, които сме писали за теб.</p>
+<p> В <a href="http://lubo.elsys-bg.org/c-programming/">сайта на Любо</a> има обща информация за курса и всичко свързано с ТУЕС. </p>
+<br>
+<p>Няма нужда да отговаряш на това съобщение, то е генерирано автоматично...</p>
+<br>
+Поздрави!"""
+
+
+EMAIL_FORGOT_PWD_TEMPLATE = """Здрасти,
+<p>Ти поиска да ти бъде сменена паролата, затова ето ти нова: <strong>{password}</strong></p>
+<p>Ако все пак си забравил кое ти е потребителското име: <strong>{username}</strong></p>
+<br>
+Поздрави!"""
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
