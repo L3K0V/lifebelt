1b693275d44dc9a18b9fff74d5c0f5134ba31578
Asen Lekov <asenlekoff@gmail.com>
2015-10-17 15:46:41 +0300

    Django logging and updated requirements

--- server/lifebelt/settings/base.py
+++ server/lifebelt/settings/base.py
@@ -128,6 +128,39 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s',
+            'datefmt': '%y %b %d, %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'celery': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'celery.log',
+            'formatter': 'simple',
+            'maxBytes': 1024 * 1024 * 100,  # 100 mb
+        },
+    },
+    'loggers': {
+        'celery': {
+            'handlers': ['celery', 'console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+from logging.config import dictConfig
+dictConfig(LOGGING)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
