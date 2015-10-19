f7e853d14164239c7eaf341b24c808bd97ae6425
Asen Lekov <asenlekoff@gmail.com>
2015-10-19 18:26:02 +0300

    Update review submission

--- server/api/assignments/tasks.py
+++ server/api/assignments/tasks.py
@@ -1,18 +1,31 @@
 from __future__ import absolute_import
 
+from django.conf import settings
+
 from celery import shared_task
 
 from api.assignments.models import AssignmentSubmission
 from api.assignments.models import SubmissionReview
 from api.members.models import Member
 
+from github3 import login
+
+LIFEBELT_BOT = getattr(settings, 'LIFEBELT_BOT_TOKEN', None)
+
 
 @shared_task
 def review_submission(submission_pk):
+    gh = login(token=LIFEBELT_BOT)
+
     submission = AssignmentSubmission.objects.get(pk=submission_pk)
-    print(submission.pull_request)
 
-    author = Member.objects.get(pk=1)
+    author = Member.objects.get(github_id=gh.me().id)
 
-    review = SubmissionReview.objects.create(
-        author=author, submission=submission, points=10, description='Compiled and running without problems!')
+    if author:
+        desc = 'Compiled and running without problems!'
+
+        review = SubmissionReview.objects.create(
+            author=author, submission=submission, points=1, description=desc)
+
+        pr = gh.pull_request(submission.pull_request.split('/')[-1])
+        pr.create_comment(desc)
