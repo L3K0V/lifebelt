2f7b3b684aa7a040e02844afd69e58a3057a5732
Asen Lekov <asenlekoff@gmail.com>
2015-10-21 19:07:50 +0300

    Added some submission review git hints

--- server/api/assignments/tasks.py
+++ server/api/assignments/tasks.py
@@ -10,25 +10,47 @@ from api.members.models import Member
 
 from github3 import login
 
-LIFEBELT_BOT = getattr(settings, 'LIFEBELT_BOT_TOKEN', None)
+LIFEBELT_BOT = getattr(settings.local, 'LIFEBELT_BOT_TOKEN', None)
 
 
 @shared_task
 def review_submission(submission_pk):
+
+    print(LIFEBELT_BOT)
+
     gh = login(token=LIFEBELT_BOT)
 
+    print(gh.me())
+
     submission = AssignmentSubmission.objects.get(pk=submission_pk)
-    repo = gh.repository(submission.pull_request.split('/')[-2], submission.pull_request.split('/')[-1])
+
+    print(submission.pull_request.split('/'))
+
+    repo = gh.repository(submission.pull_request.split('/')[-4], submission.pull_request.split('/')[-3])
 
     author = Member.objects.get(github_id=gh.me().id)
 
-    if author:
+    if True:
         desc = 'Compiled and running without problems!'
 
         review = SubmissionReview.objects.create(
             author=author, submission=submission, points=1, description=desc)
 
+        course = submission.assignment.course
+        course_dir = '{}/{}/{}'.format(getattr(settings, 'GIT_ROOT', None), course.year, course.initials)
+
         # TODO: actual git work! patch, compile, test
+
+        repo = git.Repo(course_dir)
+        o = repo.remotes.origin
+        o.pull()
+
+        repo.checkout('HEAD', b='review#{}'.format(submission.id))
+        repo.git.am('-s', 'patch file.patch')
+
+        # ... hw.py
+
+        repo.git.checkout(D='review#{}'.format(submission.id))
 
         pr = repo.pull_request(submission.pull_request.split('/')[-1])
         pr.create_comment(desc)
